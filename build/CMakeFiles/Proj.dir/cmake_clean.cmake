b6d9946b3852e91c2e8cf40f34ed31071ab54c43
Bietola <dincio.montesi@gmail.com>
2021-03-17 10:52:16 +0100

    First commit

---
+++ build/CMakeFiles/Proj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj.dir/src/main.c.o"
+  "Proj"
+  "Proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
